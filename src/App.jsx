952285fb04aff591b4198821ae2231f0235c1d04
coxmd <coxmusyoki@gmail.com>
2022-12-24 20:48:45 +0300

    Added an input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatial" />
-    <Pet name="Doink" animal="cat" breed="Mixed" />
-  </div>
+  return(
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams />
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
