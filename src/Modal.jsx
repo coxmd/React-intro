b75963fe6a3fdbfe791641ae5cfc8d65366a9874
coxmd <coxmusyoki@gmail.com>
2022-12-28 14:14:28 +0300

    Implemented adopting a pet functionality using useContext

---
+++ src/Modal.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+    const elRef = useRef(null);
+    if (!elRef.current) {
+        elRef.current = document.createElement('div');
+    }
+
+    useEffect(() => {
+        const modalRoot = document.getElementById("modal");
+        modalRoot.appendChild(elRef.current);
+
+        return () => modalRoot.removeChild(elRef.current);// IMplementation of component will unmount in class based components
+    }, []);
+
+    return createPortal(<div>{children}</div>, elRef.current)
+}
+
+export default Modal;
