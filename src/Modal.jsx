427b7b5888d1a9d4688d14bbb116df962f626255
coxmd <coxmusyoki@gmail.com>
2022-12-29 14:56:25 +0300

    Added a basic transition after clicking the submit button

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
+const Modal = ({ children }) => { 
     const elRef = useRef(null);
     if (!elRef.current) {
         elRef.current = document.createElement('div');
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,73 +1,91 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useTransition } from "react";
 import { useQuery } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
-const ANIMALS  = ["bird", "cat", "dog", "rabbit", "reptile"];
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 import fetchSearch from "./fetchSearch";
 
 const SearchParams = () => {
-    const [requestParams, setRequestParams] = useState({
-        location: "",
-        animal: "",
-        breed: "",
-    });
-    const [animal, setAnimal] = useState("");
-    const [breeds] = useBreedList(animal);
-    const [adoptedPet] = useContext(AdoptedPetContext);
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+  const [adoptedPet] = useContext(AdoptedPetContext);
+  const [isPending, startTransition] = useTransition();
 
-    const results = useQuery(["search", requestParams], fetchSearch);
-    const pets = results?.data?.pets ?? [];
-    
-    return (
-        <div className="search-params">
-            <form onSubmit={e => {
-                e.preventDefault();
-                const formData = new FormData(e.target);
-                const obj = {
-                    animal: formData.get("animal") ?? "",
-                    breed: formData.get("breed") ?? "",
-                    location: formData.get("location") ?? "",
-                };
-                setRequestParams(obj);
-            }}>
-                {
-                    adoptedPet ? (
-                        <div className="pet image-container">
-                            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
-                        </div>
-                    ) : null
-                }
-                <label htmlFor="location">
-                    Location
-                    <input name="location" id="location" placeholder="location" />
-                </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select id="animal" value={animal} onChange={e => {
-                        setAnimal(e.target.value);
-                    }}
-                    >
-                        <option />
-                        {ANIMALS.map((animal) => (
-                            <option key={animal}>{animal}</option>
-                        ))}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    Breed
-                    <select id="breed" disabled={breeds.length === 0} name="breed">
-                        <option />
-                        {breeds.map((breed) => (
-                            <option key={breed}>{breed}</option>
-                        ))}
-                    </select>
-                </label>
-                <button>Submit</button>
-            </form>
-            <Results pets={pets} />
-        </div>
-    );
-}
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            breed: formData.get("breed") ?? "",
+            location: formData.get("location") ?? "",
+          };
+          startTransition(() => {
+            setRequestParams(obj);
+          });
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
+        <label htmlFor="location">
+          Location
+          <input name="location" id="location" placeholder="location" />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            name="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select id="breed" disabled={breeds.length === 0} name="breed">
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        {isPending ? (
+          <div className="mini loading-pane">
+            <h2 className="loader">🌀</h2>
+          </div>
+        ) : (
+          <button>Submit</button>
+        )}
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
 
 export default SearchParams;
